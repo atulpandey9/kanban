29a5e079ca3811e0ce7f01b5e6bf1e40cdac16e7
AP <atul3pandey5@gmail.com>
2026-04-21 20:48:05 +0530

    added local storage

--- script.js
+++ script.js
@@ -5,12 +5,60 @@ const columns=[todo,progress,done];
 let taskdata={}
 let dragitem = null;
 
+function loadTasks() {
+    const savedData = JSON.parse(localStorage.getItem("task"));
+
+    if (!savedData) return;
+
+    columns.forEach(col => {
+        if (savedData[col.id]) {
+            savedData[col.id].forEach(task => {
+                const div = document.createElement("div");
+                div.classList.add("task");
+                div.setAttribute("draggable", "true");
+
+                div.innerHTML = `
+                    <h2>${task.title}</h2>
+                    <p>${task.desc}</p>
+                    <button>Delete</button>
+                `;
+
+                addDragEvent(div);
+                col.appendChild(div);
+            });
+        }
+
+        const count = col.querySelector(".right");
+        count.innerText = col.querySelectorAll(".task").length;
+    });
+}
+
+
+  function saveTask(){
+ columns.forEach(col => {
+    const count = col.querySelector(".heading .right");
+    const taskList = col.querySelectorAll(".task");
+
+    taskdata[col.id] = Array.from(taskList).map(t => {
+        return {
+            title: t.querySelector("h2").innerText,
+            desc: t.querySelector("p").innerText
+        };
+    });
+
+    localStorage.setItem("task", JSON.stringify(taskdata));
+    count.innerText = taskList.length;
+   
+});
+    }
+
 function addDragEvent(task) {
     task.addEventListener("dragstart", () => {
         dragitem = task;
     });
 }
-const task=document.querySelectorAll(".task").forEach(addDragEvent);
+const tasks = document.querySelectorAll(".task");
+tasks.forEach(addDragEvent);
 
 
 function addDragEventoncolumn(column) {
@@ -38,6 +86,7 @@ function addDragEventoncolumn(column) {
             count.innerText=task.length 
 
         })
+        saveTask()
     });
 }
 
@@ -79,22 +128,9 @@ const columns = document.querySelectorAll(".task-coulmn");
 
     addDragEvent(div);
     todo.appendChild(div);
-
-    columns.forEach(col => {
-        const count = col.querySelector(".heading .right");
-        const tasks = col.querySelectorAll(".task");
-                       
-        taskdata[col.id] = Array.from(task).map(t => {
-            return{
-            title: t.querySelector("h2").innerText,
-            desc: t.querySelector("p").innerText
-            }
-        });
-  localStorage.setItem("task", JSON.stringify(taskdata));
-        count.innerText = tasks.length;
-    });
- 
-  
-
+    saveTask()
+    
     modal.classList.remove("active");
 });
+
+loadTasks();
